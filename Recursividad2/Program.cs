d8fa3ec7b296435d5247d87408dcad238b653c8c
Your Name <miguelangel.gonzalez.rz@gmail.com>
2021-06-22 11:27:55 -0400

    Segunda entrada

--- Recursividad2/Program.cs
+++ Recursividad2/Program.cs
@@ -6,7 +6,69 @@ namespace Recursividad2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine(Sum(5).ToString());
+            Console.WriteLine(Fact(10).ToString());
+            Console.WriteLine(Mcd(5,10).ToString());
+
+
+        }
+
+        private static long Mcd(long a, long b)
+        {
+
+            if (b == 0)
+            {
+
+                return a;
+
+            }
+            else
+            {
+
+
+                return Mcd(b, a % b);
+            }
+
+
+
+
+        }
+
+        private static long Fact(long F)
+        {
+
+            long R = 1;
+            if (F > 0) 
+            {
+
+                R = F * Fact(F - 1);
+
+
+            }
+            return R;
+
+
+        }
+
+        private static long Sum(long S)
+        {
+
+            if (S == 1)
+            {
+
+                return S;
+
+
+
+            }
+            else 
+            {
+
+                return S + Sum(S - 1);
+            }
+
+
         }
     }
 }
